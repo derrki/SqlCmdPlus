8c64150d7cc0d0270e8169f9167f92263be85bed
Roman Kaminski <derrki@gmaol.com>
2018-05-22 13:04:29 +0300

    1. Перший тестовий коміт

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * 1. Make it work. 2. Make it right. 3. Make it fast
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start project");
+    }
+}
