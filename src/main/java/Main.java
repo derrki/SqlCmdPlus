f4652e19b2cb40b92913952da7868c1998e5219d
Roman_Kaminskiy <derrki@gmail.com>
2018-05-23 19:30:12 +0300

    8. Просто формат коду.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,14 @@
 import java.sql.SQLException;
-
 /**
  * 1. Make it work. 2. Make it right. 3. Make it fast
  */
 public class Main {
-
 
     public static void main(String[] args){
 
         String HOST_POSTGRE = "jdbc:postgresql://127.0.0.1:5432/sqlcmd";
         String USERNAME_POSTGRE = "postgres";
         String PASSWORD_POSTGRE = null;
-
 
         String HOST_MYSQL = "jdbc:mysql://localhost:3306/sqlcmd"+
                 "?verifyServerCertificate=false"+
@@ -22,8 +19,6 @@ public class Main {
                 "&serverTimezone=UTC";
         String USERNAME_MYSQL = "root";
         String PASSWORD_MYSQL = "root";
-
-
 
        DataBaseManager dataBaseManager = new DataBaseManager();
         try {
